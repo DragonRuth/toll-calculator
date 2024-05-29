605a9d60b8afdf51be0c51ea27110dd40198b786
Dragon Ruth <DragonRuth13@gmail.com>
2024-05-29 19:21:15 +0200

    style: run black styling

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
-
 from fastapi import FastAPI, Query
 from typing import List
 from datetime import datetime
 from enum import Enum
 import app.toll_calculator as toll_calculator
 
-VehicleTypes = Enum('VehicleTypes',  {vehicle: vehicle for vehicle in toll_calculator.POSSIBLE_VEHICLES}, type=str)
+VehicleTypes = Enum(
+    "VehicleTypes",
+    {vehicle: vehicle for vehicle in toll_calculator.POSSIBLE_VEHICLES},
+    type=str,
+)
 
 app = FastAPI()
+
 
 @app.get("/GetTollFee")
 def get_toll_fee(vehicle: VehicleTypes, dates: List[datetime] = Query(None)):
     return toll_calculator.get_toll_fee(vehicle, dates)
-
